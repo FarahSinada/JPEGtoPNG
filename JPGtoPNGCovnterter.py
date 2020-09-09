bd04b64d6adec009a742982c30b4972b350890eb
Raven <farahmakram@gmail.com>
2020-09-09 19:42:19 +0100

    images for conversion

--- JPGtoPNGCovnterter.py
+++ JPGtoPNGCovnterter.py
@@ -2,13 +2,21 @@ import sys
 import os
 from PIL import Image
 
-# grab first and second arguments
+# using sys grab first and second arguments
+# enter folder names like this: folder\ new\
 current_folder = sys.argv[1]
 new_folder = sys.argv[2]
 
-# check if new/ exists, if not, create it
-# new change
+print(current_folder)
+print(new_folder)
+# use os to grab path, check if new\ folder exists, if not, create it
 
-# loop through pokedex,
+try:
+    pass
+
+except:
+    pass
+
+# loop through pokedex folder
 # convert to png
 # save to the new folder
