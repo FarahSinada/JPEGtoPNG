cb6e8adce709547a08f8cf3545a1d72ae3a82eb6
Raven <farahmakram@gmail.com>
2020-09-10 13:11:32 +0100

    added file things

--- JPGtoPNGCovnterter.py
+++ JPGtoPNGCovnterter.py
@@ -18,11 +18,10 @@ current_folder = sys.argv[1]
 new_folder = sys.argv[2]
 
 # use os to grab path, check if new\ folder exists, if not, create it
-# os.getcwd() gets full path, but if folder is in current project folder, just use .\ for path
 # path = os.getcwd()
+# os.getcwd() gets full path, but if folder is in current project folder, just use .\ for path
 
 check_folders(current_folder, new_folder)
-
 
 try:
     pass
@@ -30,6 +29,14 @@ try:
 except:
     pass
 
+with open(f'.{current_folder}',mode='r') as my_folder:
+    test = Image.open('.\Pikachu.jpg')
+
 # loop through pokedex folder
+# for images in current_folder:
+#
+#     img = images.open(f'.\{current_folder}.jpg')
+#     img.show()
+
 # convert to png
 # save to the new folder
