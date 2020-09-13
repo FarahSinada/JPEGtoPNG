c69c4b294bd7b7caae5ef3261ed992f47851e3cc
Raven <farahmakram@gmail.com>
2020-09-13 20:37:00 +0100

    solve file error issues with scandir

--- JPGtoPNGCovnterter.py
+++ JPGtoPNGCovnterter.py
@@ -32,26 +32,30 @@ except PermissionError as err:
     pass
 
 path_cur = os.path.join('.\\', current_folder)
-path_new = os.path.join('.\\', new_folder)
+path_new = os.path.join(os.getcwd(), new_folder)
 # print(path)
 
 with os.scandir(path_cur) as my_folder:
+    # loop through pokedex folder for images
     for entry in my_folder:
         if entry.name.endswith(".jpg") and entry.is_file():
             print(entry.name, entry.path)
-            # img = Image.open(entry)
-            # new_img = img.save(path_new,'.png')
+            # entry is object of type nt.DirEntry
+            # print(type(entry))
+            # entry.path is of type str
+            img = Image.open(entry.path)
 
+            print(path_new)
+
+            # convert to png, save to the new folder
+            with os.scandir(path_new) as png_folder:
+                new_img = img.save(entry.name, 'PNG')
 
 
 # with open(f'.\{current_folder}',mode='r') as my_folder:
 #     test = Image.open('.\Pikachu.jpg')
 
-# loop through pokedex folder
-# for images in current_folder:
-#
-#     img = images.open(f'.\{current_folder}.jpg')
 #     img.show()
 
-# convert to png
-# save to the new folder
+# FileNotFoundError: [Errno 2] No such file or directory: usually when trying to open directory
+# can be solved by using with os.scandir(folder path) as my_folder:
